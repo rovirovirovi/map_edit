ae900468bf0a8879a13da229bc9cac995a2c95c0
MisterYO1122 <vali_rosca99@yahoo.com>
2017-01-13 14:38:16 +0200

    Update tileManager.lua

--- tileManager.lua
+++ tileManager.lua
@@ -4,7 +4,7 @@
 #   Copyright (C) 2016-2017 Rosca Valentin
 #
 #   This project is free software; you can redistribute it and/or modify it
-#   under the terms of the GNU v3 license. See LICENSE.md for details.
+#   under the terms of the MIT license. See LICENSE.md for details.
 ]]
 
 require "camera"
