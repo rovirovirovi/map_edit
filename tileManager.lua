23e92678dcf26422a7b0af7198b867c6028f6020
Rosca Valentin <vali_rosca99@yahoo.com>
2017-01-14 23:58:36 +0200

    Update tileManager.lua

--- tileManager.lua
+++ tileManager.lua
@@ -1,5 +1,5 @@
 --[[
-	#   Copyright (C) 2017-2018 Rosca Valentin
+	#   Copyright (C) 2017 Rosca Valentin
  	#
  	#   This project is free software; you can redistribute it and/or modify it
 	#   under the terms of the MIT license. See LICENSE.md for more details.
